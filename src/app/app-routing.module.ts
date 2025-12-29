cc659006f9e7360d187c823ee9c4736a7001bb03
Cipher-13 <aihab6087@gmail.com>
2025-12-29 20:39:27 +0200

    App-Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./Features/auth/auth.module').then(m => m.AuthModule) }, { path: 'products', loadChildren: () => import('./Features/products/products.module').then(m => m.ProductsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
